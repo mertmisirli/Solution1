b49f51604dd1539e435449a28bd54e231a56f4ea
Furkan Mert Mısırlı <misirlifu@mef.edu.tr>
2023-10-16 14:51:42 +0300

    update

---
+++ Productify/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Productify.Domain.Entities;
+using Productify.Persistent.Contexts;
+
+namespace Productify.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductifyDbContext _context;
+
+        public ProductController()
+        {
+            _context = new();
+        }
+
+        public IActionResult GetAll()
+        {
+            return View(_context.Products.ToList());
+        }
+
+        
+
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult AddProduct(string productName)
+        {
+            _context.Products.Add(new Product(productName));
+            _context.SaveChanges();
+
+            return View();
+        }
+    }
+}
